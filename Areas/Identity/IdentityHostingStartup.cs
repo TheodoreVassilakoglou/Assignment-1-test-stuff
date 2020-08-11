98038225b1d3178b70ce4ceb1bc78cc4c00363af
TheodoreVassilakoglou <theobear@live.com.au>
2020-08-11 22:50:16 +0930

    This took me 8 hours of crying and smacking my head on the table. I had to restart this 8 times. Luckily i accidentally did the write thing and failed to success. This succesfully allows email registration confirmmation :). 11/08/2020 1pm - 8pm

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Sad.Data;
+
+[assembly: HostingStartup(typeof(Sad.Areas.Identity.IdentityHostingStartup))]
+namespace Sad.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
